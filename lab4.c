4717cc287b6960cd03900183cbfdf308af42fcfa
r4yyanx <k256557@nu.edu.pk>
2026-03-02 12:09:24 +0500

    Add files via upload

---
+++ lab4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    float attendance;
+ 
+    printf("Enter attendance percentage: ");
+    scanf("%f", & attendance);
+ 
+    if (attendance >= 75) {
+        printf("Eligible for Exam\n");
+    } else {
+        printf("Not Eligible for Exam");
+    }
+ 
+    return 0;
+}
